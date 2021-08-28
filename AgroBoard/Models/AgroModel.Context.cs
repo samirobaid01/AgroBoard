1e09621b4aada5951d11779516951c2a24283e29
samir obaid <samir_obaid@live.com>
2021-08-28 15:00:15 +0500

    model folder updated

--- AgroBoard/Models/AgroModel.Context.cs
+++ AgroBoard/Models/AgroModel.Context.cs
@@ -18,6 +18,7 @@ namespace AgroBoard.Models
         public AgroboardDataEntities()
             : base("name=AgroboardDataEntities")
         {
+            this.Configuration.ProxyCreationEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
